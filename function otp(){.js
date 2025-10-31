b499a4d2a49a1a73b88df6b31594396136c7cb4a
Boaz001-ops <boaz0391@gmail.com>
2025-10-31 03:09:30 +0300

    Add files via upload

---
+++ function otp(){.js
@@ -0,0 +1,13 @@
+function otp(){
+    const email =document.getElementById('email');
+    cons otpverify = document.getElementByClassName('otpverify')[0]
+
+Email.send({
+    SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);}
